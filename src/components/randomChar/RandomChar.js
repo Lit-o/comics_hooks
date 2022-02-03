2cc9ad1c709d7775f16299de5263a22727650a95
Boris <boris12lit@gmail.com>
2022-02-03 17:57:29 +0300

    reworked RandomChar.js into a func with hooks

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import ComicsAPI from '../../api/ComicsAPI';
 import { Error } from '../error/Error';
 
@@ -6,77 +6,45 @@ import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
 import Preloader from '../spinner/Preloader';
 
-class RandomChar extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     // первый вызов функции не очень хорошо делать в конструкторе
-    //     this.updateChar()
-    //     // this.state = {
-    //     // }
-    // }
+const RandomChar = () => {
 
-    // синтаксис полей классов
-    state = {
-        char: {},
-        isLoading: true,
-        isError: false,
-    }
-        // name: null,
-        // description: null,
-        // thumbnail: null,
-        // homepage: null,
-        // wiki: null
+    const [char, setChar] = useState({})
+    const [isLoading, setIsLoading] = useState(true)
+    const [isError, setIsError] = useState(false)
 
-    server = new ComicsAPI();
+    const server = new ComicsAPI();
 
-    componentDidMount() {
-        this.updateChar()
+    useEffect(() => {
+        updateChar()
+    // eslint-disable-next-line
+    }, [])
+
+    const charLoaded = (char) => {
+        setChar(char)
+        setIsLoading(false)
     }
 
-    // componentWillUnmount() {
-    //     console.log('unmount Random char')
-    // }
-
-    charLoaded = (char) => {
-        // this.setState({char: char})
-        this.setState({char, isLoading: false})
+    const errorCatched = () => {
+        setIsLoading(false)
+        setIsError(true)
     }
 
-    errorCatched = () => {
-        this.setState({
-            isLoading: false, 
-            isError: true
-        })
-    }
-
-    updateChar = () => {
-        this.setState({
-            isLoading: true, 
-            isError: false
-        })
-        // const id = 1011005
+    const updateChar = () => {
+        setIsLoading(true)
+        setIsError(false)
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000)
-        // console.log(this.server.getCharacter(id))
-        this.server
+        server
             .getCharacter(id)
-            .then(this.charLoaded)
-            .catch(this.errorCatched)
-            // .then(res => {
-            //     this.setState(res)
-            // })
+            .then(charLoaded)
+            .catch(errorCatched)
     }
 
-    render() {
-        // const { name, description, thumbnail, homepage, wiki } = this.state.char;
-        // const {char: {name, description, thumbnail, homepage, wiki}, isLoading} = this.state;
-        const {char, isLoading, isError} = this.state;
         const errorMessage = isError ? <Error/> : null;
         const spinner = isLoading && <Preloader/>;
         const content = !(isLoading || isError) && <ViewRandomChar char={char}/>;
        
         return (
             <div className="randomchar">
-                {/* {isLoading ? <Preloader/> : <ViewRandomChar char={char}/>}   */}
                 {errorMessage}{spinner}{content}
                 <div className="randomchar__static">
                     <p className="randomchar__title">
@@ -86,7 +54,7 @@ class RandomChar extends Component {
                     <p className="randomchar__title">
                         Or choose another one
                     </p>
-                    <button className="button button__main" onClick={this.updateChar}>
+                    <button className="button button__main" onClick={updateChar}>
                         <div className="inner">try it</div>
                     </button>
                     <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
@@ -94,14 +62,10 @@ class RandomChar extends Component {
             </div>
         )
     }
-}
 
-// const ViewRandomChar = (props) => {    
-//     const {name, description, thumbnail, homepage, wiki} = props.char
 const ViewRandomChar = ({char}) => {    
     const {name, description, thumbnail, homepage, wiki} = char
     const notFoundImgLink = 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg';
-    // const isObjectFit = ;
     return (
         <div className="randomchar__block">
             <img src={thumbnail} alt="Random character" className="randomchar__img" style={(thumbnail === notFoundImgLink) ? {objectFit: 'contain'} : {objectFit: 'cover'}} />
