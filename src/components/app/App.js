9fe5954f8e3a79a93e3950f2185f9d65c0fa861d
Boris <boris12lit@gmail.com>
2022-02-09 01:56:43 +0300

    cleaning

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { lazy, Suspense } from 'react'
 
 import AppHeader from "../appHeader/AppHeader";
